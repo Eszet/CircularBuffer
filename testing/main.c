cd621ada24744a293bf621297fe4d81ba976332c
Johannes Lieder <jlieder@Mac-mini.local>
2012-09-22 09:54:41 +0200

    Initial Commit

---
+++ testing/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  testing
+//
+//  Created by Johannes Lieder on 22.09.12.
+//  Copyright (c) 2012 Johannes Lieder. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
---
+++ testing/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  testing
+//
+//  Created by Johannes Lieder on 22.09.12.
+//  Copyright (c) 2012 Johannes Lieder. All rights reserved.
+//
+
+#include <CommandLineTestRunner.h>
+
+int main(int argc, const char * argv[])
+{
+    return RUN_ALL_TESTS(argc, argv);
+}
